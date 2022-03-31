e87b28bbe78d4a9722cfc31a1739b13c83a942b5
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-03-31 16:58:25 +0000

    Completed challenge

--- main.rb
+++ main.rb
@@ -1,1 +1,13 @@
-puts "Hello, World!"
+puts "Enter your first name"
+first_name = gets.chomp
+
+puts "Enter your last name"
+last_name = gets.chomp
+
+full_name = first_name + " " + last_name
+
+puts "Your full name is #{full_name} "
+
+puts "Your full name reversed is #{full_name.reverse} " 
+
+puts "Your full name has #{full_name.length - 1} characters"
